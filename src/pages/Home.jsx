2133f142a793205e731e80a613eb4e2b2b0bb3d4
adriana_ardila <adriana2014lucia@outlook.com>
2020-11-19 15:24:36 -0500

    Upload files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import Discount from '../components/Discount'
-import Information from '../components/Information'
+import Discount from "../components/Discount";
+import Information from "../components/Information";
+import Header from "../components/Header";
 
-const Home =()=>{
-  return(
+const Home = () => {
+  return (
     <div>
-      <Information/>
-      <Discount/>
+      <Header />
+      <Information />
+      <Discount />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import Form from '../components/Form'
-import '../assets/styles/main.css'
+import Form from "../components/Form";
+import "../assets/styles/main.css";
+import Header from "../components/Header";
 
-const Register = () =>{
-  return(
+const Register = () => {
+  return (
     <div>
-      <Form/>
+      <Form />
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
