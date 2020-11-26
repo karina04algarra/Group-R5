74cf9ccb43180f200af98d3a80e898785e0c6dd5
adriana_ardila <adriana2014lucia@outlook.com>
2020-11-26 16:12:53 -0500

    Some changes

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -6,15 +6,15 @@ class Discount extends Component {
   render() {
     return (
       <>
-        <div>
+        <div className="discount">
           <p>Hemos entregado mas de</p>
           <h2>$</h2>
           <p>en descuentos</p>
-        </div>
-        <div>
-          <p>Placa del vehiculo</p>
-          <input type="text" placeholder="HTR616" />
-          <Link to="/register"> Cotizar Gratis</Link>
+          <div>
+            <p>Placa del vehiculo</p>
+            <input type="text" placeholder="HTR616" />
+            <Link to="/register"> Cotizar Gratis</Link>
+          </div>
         </div>
       </>
     );
