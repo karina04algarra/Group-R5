72d0862e8080900d569a6e50d43b3ef93c9a8bf6
adriana_ardila <adriana2014lucia@outlook.com>
2020-12-10 14:59:23 -0500

    Other changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./src/routes/App";
+import App from "./routes/App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
